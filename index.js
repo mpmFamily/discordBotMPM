8c4a45739790960e1d10487d13e9883025e2306b
YuriASPassarini <69130219+YuriASPassarini@users.noreply.github.com>
2020-08-03 18:41:15 -0300

    bot responses

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 client.login(config.token)
 
-
+client.on("message", msg =>
+{
+    let responde = 
+        {
+            "oi": "ola vadia",
+            "bom dia": "bom dia puta",
+            "ola": "ola prostiranha",
+            "navas macaco": "=true",
+        }
+    if (msg.author.bot) {return;}
+   
+    if (responde[msg.content])
+    {
+        msg.channel.send(responde[msg.content]);
+    }
+})
