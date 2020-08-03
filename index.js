3e0e423a1028497c4f7af054354e4c79d9837448
Rodrigo Petrelli <69090675+Diguidiin@users.noreply.github.com>
2020-08-03 02:27:36 -0300

    update no index.js
    Meu bot agr responde o salve, e ja coloquei uma pré função na tabela, eu chuto que deve ser o mesmo código pra ele falar algo, então ja deixei pronto

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const client = new Discord.Client()
 const config = require('./config/config.json')
 client.login(config.token)
 client.on('ready', () => { console.log('Bot Ligado') });
+//sempre bom ter um bot educado
+client.on('message', msg => {
+    if (msg.content === 'salve') {
+      msg.reply('Salve irmão!');
+    }
+  });
+  //usar ".tabela" pra ele mandar a tabela (por enquanto só a frase xD)
+  client.on('message', msg2 => {
+    if (msg2.content === '.tabelaDigo') {
+        msg2.reply('É pra já campeão!');
+    }
+});
