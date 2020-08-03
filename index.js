4d1cc30af0f514411db8ff2371226f3d38a9cded
Vinicius Rangel <vr3lxrd@outlook.com>
2020-08-02 23:01:27 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const config = require('./config/config.json')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+
+client.on('message', (message) => { 
+    
+})
+
+
+client.login(config.token)
+
+console.log(config.prefix)
