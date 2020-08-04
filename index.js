aaa42c557e5d60bd5fbb037e25ea7ba6eba9fa18
Rodrigo Petrelli <69090675+Diguidiin@users.noreply.github.com>
2020-08-04 00:34:13 -0300

    update index.js
    atualização no status do bot

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const config = require('./config/config.json')
+
 client.login(config.token)
-client.on('ready', () => { console.log('Bot Ligado') });
-//sempre bom ter um bot educado
-client.on('message', msg => {
-    if (msg.content === 'salve') {
-      msg.reply('Salve irmão!');
-    }
-  });
-  //usar ".tabela" pra ele mandar a tabela (por enquanto só a frase xD)
-  client.on('message', msg2 => {
-    if (msg2.content === '.tabelaDigo') {
-        msg2.reply('É pra já campeão!');
-    }
-});
+
+client.on('ready', () => {
+    console.log('Bot Ligado')
+    client.user.setActivity('Brasileirão 20/21', { type: 'WATCHING' })
+})
+    //sempre bom ter um bot educado 
+    client.on ('message', msg => { if (msg.content === 'salve')
+     { msg.reply('Salve irmão!') } });
+
+    //usar ".tabelaDigo" pra ele mandar a tabela (por enquanto só a frase xD)
+
+    client.on('message', msg2 => { 
+        if (msg2.content === '.tabelaDigo') { 
+            msg2.reply('É pra já campeão!') 
+        } 
+    })
