72eaeb9ce2c41c564fd46c0e7eb2e0560749b4fc
LAzz1 <64385770+LAzz1@users.noreply.github.com>
2020-08-05 17:08:05 -0300

    tabelas
    tabelas de time 2019/2020 e inicio das individuais

--- Hackaton/app.js
+++ Hackaton/app.js
@@ -4,6 +4,9 @@ const config = require('./config/config.json')
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const data = require('./data.js')
+const prefix = "!"
+let ano = [2019, 2020];
+let nomes = ['flamengo', 'santos', 'palmeiras']
 
 bot.login(config.token);
 
@@ -14,20 +17,28 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
     let responde = {
-        ".bot": "to on amigao, use >> .comandos << para mais informacoes",
+        "!bot": "to on amigao, use >> .comandos << para mais informacoes",
         "oi": "salve",
-        ".comandos": ".teste  .status  .comandos  .gay",
-        ".help":"aqui nois usa .comandos ok",
-        ".gay": `claro que eh o ${msg.author}`,
+        "!comandos": ".teste  .status  .comandos  .gay",
+        "!help":"aqui nois usa .comandos ok",
+        "!gay": `claro que eh o ${msg.author}`,
         "salve": "salvado pae",
-        ".pinto": "calma to desenvolvendo"
+        "!pinto": "calma to desenvolvendo"
     }
 
-    if (msg.content.startsWith('.status')) {
+    if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${ano[0]}`) || msg.content.toLowerCase().startsWith(`${config.prefix}time${ano[0]}`)) {
         data.run(msg, bot)
     }
-    else if (msg.content.startsWith('.teste')) {
+    else if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${ano[1]}`) || msg.content.toLowerCase().startsWith(`${config.prefix}time${ano[1]}`)) {
         data.run(msg, bot)
+    }
+
+    else if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${nomes[0]}`)) {
+        data.run(msg, bot)
+    }
+
+    else if (msg.content.startsWith(`${config.prefix}time`,)) {
+        msg.channel.send('esse ano ainda nao esta disponivel, tente novamente em breve')
     }
 
     if (msg.author.bot) { return; }
--- Hackaton/data.js
+++ Hackaton/data.js
@@ -1,28 +1,70 @@
 const axios = require('axios');
 const Discord = require('discord.js');
+const cheerio = require('cheerio');
+const { MessageEmbed } = require('discord.js');
 const bot = new Discord.Client();
+const url = 'https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/'
+let ano = [2019, 2020]
+let nomes = ['flamengo', 'santos', 'palmeiras']
+
+exports.run = (msg, bot) => {
+    if (msg.content.toLowerCase().startsWith(`!time ${ano[0]}`) || msg.content.toLowerCase().startsWith(`!time${ano[0]}`)){
+        axios.get(url + 2019).then(response => {
+            const html = (response.data);
+            const $ = cheerio.load(html);          
+            const embedTime = new MessageEmbed();
+            let timesTable = $('.expand-trigger > td > .hidden-xs ')
+
+            embedTime.setTitle('Times brasileirao 2019')
+            for (let i = 0; i <= 19; i++) {
+                const individualTeam = timesTable[i].children[0]
+                const time = individualTeam.data
+                embedTime.addField(time, `${i + 1} posicao`)
+            }
+            embedTime.setColor('#DC143C');
+            msg.channel.send(embedTime)
+           
+        })
+
+        
+
+    }
+
+    else if (msg.content.toLowerCase().startsWith(`!time ${ano[1]}`) || msg.content.toLowerCase().startsWith(`!time${ano[1]}`)){
+        axios.get(url + 2020).then(response => {
+            const html = (response.data);
+            const $ = cheerio.load(html);
+            const embedTime = new MessageEmbed();
+            let timesTable = $('.expand-trigger > td > .hidden-xs')
+
+            embedTime.setTitle('Times brasileirao 2020')
+            embedTime.setDescription('sem posicao definida')
+            for (let i = 0; i <= 19; i++) {
+                const individualTeam = timesTable[i].children[0]
+                const time = individualTeam.data
+                embedTime.addField(time, '-------------')
+            }
+            embedTime.setColor('#FFD700');
+            msg.channel.send(embedTime)
+        })
+    }
+
+    if (msg.content.toLowerCase().startsWith(`!time ${nomes[0]}`)) {
+        axios.get(url + 2019).then(response => {
+            const html = (response.data);
+            const $ = cheerio.load(html);
+            const embedTime = new MessageEmbed();
+            let pontos = $('.expand-trigger > th ')
+            let partidas = $('.expand-trigger > td')
 
 
-exports.run = (msg, bot) => { 
-    axios.get('https://reqres.in/api/users').then(response => {
-
-        if (msg.content.startsWith('.teste')) {
-            console.log(response.status);   
-
-            let testando = response.data
-
-            if (msg.author.bot) { return; }
-            else { msg.channel.send(`${msg.author} aqui esta ${testando}`); }  
-        }
-
-        if (msg.content.startsWith('.status')) {
-            let estatos = response.status
-
-            if (msg.author.bot) { return; }
-            else { msg.channel.send(`status de ${estatos}`); }  
-
-        }
-    })
+            embedTime.setTitle('Flamengo')
+            embedTime.addField(pontos[0].children[0].data, 'pontos')
+            embedTime.setColor('#000000')
+            msg.channel.send(embedTime)
+        })
+    }
+          
 };
 
 const sendData = () => {
