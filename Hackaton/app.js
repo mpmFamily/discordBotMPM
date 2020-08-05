51e7bb9bb89867dc425293fbaa5fe8bb415a173d
LAzz1 <64385770+LAzz1@users.noreply.github.com>
2020-08-05 19:18:12 -0300

    individual times

--- Hackaton/app.js
+++ Hackaton/app.js
@@ -7,6 +7,8 @@ const data = require('./data.js')
 const prefix = "!"
 let ano = [2019, 2020];
 let nomes = ['flamengo', 'santos', 'palmeiras']
+let aleatorio = Math.random();
+
 
 bot.login(config.token);
 
@@ -17,14 +19,20 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
     let responde = {
-        "!bot": "to on amigao, use >> .comandos << para mais informacoes",
+        "!bot": "to on amigao, use >> !comandos << para mais informacoes",
         "oi": "salve",
-        "!comandos": ".teste  .status  .comandos  .gay",
-        "!help":"aqui nois usa .comandos ok",
+        "!comandos": "!pinto\n!gay\n!bot\n",
+        "!help":"aqui nois usa !comandos ok",
         "!gay": `claro que eh o ${msg.author}`,
         "salve": "salvado pae",
-        "!pinto": "calma to desenvolvendo"
+       
     }
+    if (msg.author.bot) { return; }
+
+    if (msg.content.toLowerCase().startsWith('!pinto')) {
+        msg.channel.send(`seu pinto tem ${aleatorio} cm`)
+    }
+
 
     if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${ano[0]}`) || msg.content.toLowerCase().startsWith(`${config.prefix}time${ano[0]}`)) {
         data.run(msg, bot)
@@ -36,12 +44,17 @@ bot.on('message', msg => {
     else if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${nomes[0]}`)) {
         data.run(msg, bot)
     }
+    else if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${nomes[1]}`)) {
+        data.run(msg, bot)
+    }
+    else if (msg.content.toLowerCase().startsWith(`${config.prefix}time ${nomes[2]}`)) {
+        data.run(msg, bot)
+    }
 
     else if (msg.content.startsWith(`${config.prefix}time`,)) {
         msg.channel.send('esse ano ainda nao esta disponivel, tente novamente em breve')
     }
 
-    if (msg.author.bot) { return; }
 
     if (responde[msg.content.toLowerCase()]) {
         msg.channel.send(responde[msg.content.toLowerCase()]);
--- Hackaton/data.js
+++ Hackaton/data.js
@@ -55,12 +55,49 @@ exports.run = (msg, bot) => {
             const $ = cheerio.load(html);
             const embedTime = new MessageEmbed();
             let pontos = $('.expand-trigger > th ')
-            let partidas = $('.expand-trigger > td')
+            let partidas = $('.expand-trigger > td ')
 
 
-            embedTime.setTitle('Flamengo')
-            embedTime.addField(pontos[0].children[0].data, 'pontos')
+            embedTime.setTitle('Flamengo - RJ (2019)')
+            embedTime.setThumbnail('https://upload.wikimedia.org/wikipedia/commons/9/93/Flamengo-RJ_%28BRA%29.png')
+            embedTime.addField(pontos[0].children[0].data, 'pontos totais')
+            embedTime.addField(partidas[1].children[0].data, 'partidas jogadas')
+            embedTime.setColor('#DC143C')
+            msg.channel.send(embedTime)
+        })
+    }
+
+    else if (msg.content.toLowerCase().startsWith(`!time ${nomes[1]}`)) {
+        axios.get(url + 2019).then(response => {
+            const html = (response.data);
+            const $ = cheerio.load(html);
+            const embedTime = new MessageEmbed();
+            let pontos = $('.expand-trigger > th ')
+            let partidas = $('.expand-trigger > td ')
+
+
+            embedTime.setTitle('Santos - SP (2019)')
+            embedTime.setThumbnail('https://pbs.twimg.com/profile_images/1201694733376118786/d96e39ad_400x400.jpg')
+            embedTime.addField(pontos[1].children[0].data, 'pontos totais')
+            embedTime.addField(partidas[1].children[0].data, 'partidas jogadas')
             embedTime.setColor('#000000')
+            msg.channel.send(embedTime)
+        })
+    }
+    else if (msg.content.toLowerCase().startsWith(`!time ${nomes[2]}`)) {
+        axios.get(url + 2019).then(response => {
+            const html = (response.data);
+            const $ = cheerio.load(html);
+            const embedTime = new MessageEmbed();
+            let pontos = $('.expand-trigger > th ')
+            let partidas = $('.expand-trigger > td ')
+
+
+            embedTime.setTitle('Palmeiras - SP (2019)')
+            embedTime.setThumbnail('https://img.elo7.com.br/product/244x194/2BF496C/matriz-de-bordado-escudo-time-de-futebol-palmeira-maquina-de-bordar.jpg')
+            embedTime.addField(pontos[2].children[0].data, 'pontos totais')
+            embedTime.addField(partidas[1].children[0].data, 'partidas jogadas')
+            embedTime.setColor('#3CB371')
             msg.channel.send(embedTime)
         })
     }
