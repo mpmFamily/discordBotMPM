32bc108719c2c179c8710fcd6fa798153efaaf87
lucasnavas <lucas-navas1@hotmail.com>
2020-08-05 12:47:04 -0300

    Index com bot ligando

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-vini viado
+const config = require('./config/config.json')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.login(config.token)
+client.on('ready', () => {console.log("COMI O DE BAIXO") 
+    client.user.setActivity('fut contra o torneiras', { type: 'PLAYING' });
+})
